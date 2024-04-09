aafd6a90c50c96eb8034355437c9a7ce39a80dad
Asif Shikder <shikdernyc@gmail.com>
2024-04-09 01:40:25 -0400

    Added Palico common package

--- packages/palico-app/src/utils/jwt.ts
+++ packages/palico-app/src/utils/jwt.ts
@@ -1,44 +1,6 @@
-import * as JWT from 'jsonwebtoken'
-import config from '../config'
-import { PreferenceStore } from './preference_store'
-
-export default class JWTAuthenticator {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  static async generateAPIJWT (payload: any, secret: string): Promise<string> {
-    return await new Promise((resolve, reject) => {
-      JWT.sign(
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        {
-          ...payload
-        },
-        secret,
-        {},
-        (err, token) => {
-          if (err ?? !token) {
-            reject(err)
-          } else {
-            resolve(token)
-          }
-        }
-      )
-    })
-  }
-
-  static async decodeJWTToken<Payload> (token: string, secret: string): Promise<Payload> {
-    const tokenPart = token.includes(' ') ? token.split(' ')[1] : token
-    return await new Promise((resolve, reject) => {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      JWT.verify(tokenPart, secret, (err, decoded: any) => {
-        if (err) {
-          reject(err)
-        } else {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-          resolve(decoded)
-        }
-      })
-    })
-  }
-}
+import { JWTAuthenticator } from "@palico-ai/common"
+import config from "../config"
+import { PreferenceStore } from "./preference_store"
 
 interface AuthorizationTokenParams {
   currentSecret: string
