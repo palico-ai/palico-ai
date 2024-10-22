25833fa4c021521cf1e3f114aa2654da6993062a
Asif Shikder <shikdernyc@gmail.com>
2024-10-22 15:49:20 -0400

    Added Exact Match Eval Metric

---
+++ packages/palico-app/src/experiments/metrics/exact_match.ts
@@ -0,0 +1,33 @@
+import { AgentRequestContent, AgentResponse } from '@palico-ai/common';
+import { EvalMetric, EvalMetricOutput } from '../types';
+import { isEqual } from 'lodash';
+
+export interface ExactMatchParams {
+  expected: Pick<AgentResponse, 'message' | 'data'>;
+}
+
+/**
+ * Checks if the response is an exact match.
+ * Returns 0 if the response is not an exact match, 1 if it is.
+ */
+export class ExactMatchMetric implements EvalMetric {
+  private params: ExactMatchParams;
+  label = 'Exact Match';
+
+  constructor(params: ExactMatchParams) {
+    this.params = params;
+  }
+
+  async evaluate(
+    _: AgentRequestContent,
+    response: AgentResponse
+  ): Promise<EvalMetricOutput> {
+    if (response.message !== this.params.expected.message) {
+      return 0;
+    }
+    if (!isEqual(response.data, this.params.expected.data)) {
+      return 0;
+    }
+    return 1;
+  }
+}
--- packages/palico-app/src/experiments/metrics/index.ts
+++ packages/palico-app/src/experiments/metrics/index.ts
@@ -1,2 +1,3 @@
 export * from './contains';
 export * from './json';
+export * from './exact_match';
