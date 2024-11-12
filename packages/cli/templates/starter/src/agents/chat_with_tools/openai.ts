5b44353a429f178c4b82b92913f50e46c2188975
Asif Shikder <shikdernyc@gmail.com>
2024-11-12 15:03:48 -0500

    Updated starter app and showing logs on Palico Studio:

--- packages/cli/templates/starter/src/agents/chat_with_tools/openai.ts
+++ packages/cli/templates/starter/src/agents/chat_with_tools/openai.ts
@@ -1,58 +1,55 @@
-import OpenAI from "openai";
-import { ChatCompletionFunction, Message } from "./types";
-import {
-  ChatCompletionTool,
-  ChatCompletionMessageParam,
-  ChatCompletionMessage,
-} from "openai/resources";
-import { logger, Tool } from "@palico-ai/app";
-import zodToJsonSchema from "zod-to-json-schema";
+import OpenAI from 'openai';
+import { ChatCompletionFunction, Message } from './types';
+import { logger, Tool } from '@palico-ai/app';
+import zodToJsonSchema from 'zod-to-json-schema';
 
 export interface CallOpenAIParams {
   messages: Message[];
   tools: Tool[];
 }
 
-const toOpenAIMessage = (message: Message): ChatCompletionMessageParam => {
+const toOpenAIMessage = (
+  message: Message
+): OpenAI.Chat.ChatCompletionMessageParam => {
   const { role, content, toolCalls, toolCallResult } = message;
   switch (role) {
-    case "user":
+    case 'user':
       if (!content) {
-        throw new Error("User message must have content");
+        throw new Error('User message must have content');
       }
       return {
-        role: "user",
+        role: 'user',
         content: content,
       };
-    case "system":
+    case 'system':
       if (!content) {
-        throw new Error("System message must have content");
+        throw new Error('System message must have content');
       }
       return {
-        role: "system",
+        role: 'system',
         content: content,
       };
-    case "tool":
+    case 'tool':
       if (!toolCallResult || !toolCallResult.id) {
-        throw new Error("Tool message must have toolCallResults and id");
+        throw new Error('Tool message must have toolCallResults and id');
       }
       return {
-        role: "tool",
+        role: 'tool',
         tool_call_id: toolCallResult.id,
         content: JSON.stringify(toolCallResult.result),
       };
-    case "assistant":
+    case 'assistant':
       if (content) {
         return {
-          role: "assistant",
+          role: 'assistant',
           content: content,
         };
       } else if (toolCalls) {
         return {
-          role: "assistant",
+          role: 'assistant',
           tool_calls: toolCalls?.map((toolcall) => ({
             id: toolcall.id,
-            type: "function",
+            type: 'function',
             function: {
               name: toolcall.name,
               arguments: JSON.stringify(toolcall.parameters),
@@ -60,14 +57,16 @@ const toOpenAIMessage = (message: Message): ChatCompletionMessageParam => {
           })),
         };
       } else {
-        throw new Error("Assistant message must have content or toolCalls");
+        throw new Error('Assistant message must have content or toolCalls');
       }
     default:
-      throw new Error("Invalid role");
+      throw new Error('Invalid role');
   }
 };
 
-const toPalicoMessage = (message: ChatCompletionMessage): Message => {
+const toPalicoMessage = (
+  message: OpenAI.Chat.ChatCompletionMessage
+): Message => {
   return {
     role: message.role,
     toolCalls: message.tool_calls?.map((toolCall) => ({
@@ -81,9 +80,9 @@ const toPalicoMessage = (message: ChatCompletionMessage): Message => {
   };
 };
 
-const formatTools = (tools: Tool[]): ChatCompletionTool[] => {
+const formatTools = (tools: Tool[]): OpenAI.Chat.ChatCompletionTool[] => {
   return tools.map((tool) => ({
-    type: "function",
+    type: 'function',
     function: {
       name: tool.name,
       description: tool.description,
@@ -95,17 +94,17 @@ const formatTools = (tools: Tool[]): ChatCompletionTool[] => {
 };
 
 export const openaiChatCompletion: ChatCompletionFunction = async (params) => {
-  logger.info("Calling OpenAI chat completion");
-  logger.info("Messages", params.messages);
+  logger.info('Calling OpenAI chat completion');
+  logger.info('Messages', params.messages);
   const { messages } = params;
   if (!process.env.OPENAI_API_KEY) {
-    throw new Error("Missing OPENAI_API_KEY in environment variables");
+    throw new Error('Missing OPENAI_API_KEY in environment variables');
   }
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo-0125",
+    model: 'gpt-3.5-turbo-0125',
     messages: messages.map(toOpenAIMessage),
     tools: formatTools(params.tools),
     max_tokens: 2000,
---
+++ packages/cli/templates/starter/src/agents/types.ts
@@ -0,0 +1,4 @@
+// config value / feature-flags that can be passed to the agent
+export interface AppConfig {
+  model?: string;
+}
